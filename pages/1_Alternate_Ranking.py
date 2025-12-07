853dc5185ba44e058832f8a20ea55a32401ef40a
Xinxue (Shawn) Qu <1990qvxinxue@gmail.com>
2025-12-07 13:51:03 -0500

    Update 1_Alternate_Ranking.py

--- pages/1_Alternate_Ranking.py
+++ pages/1_Alternate_Ranking.py
@@ -57,7 +57,7 @@ if filtered.empty:
 
 # (c) TIME WINDOW FILTER (based on CloseDate)
 # Ensure CloseDate is treated as datetime
-st.write("Columns:", df.columns.tolist())
+st.write("Columns:", filtered.columns.tolist())
 
 filtered["CloseDate"] = pd.to_datetime(filtered["CloseDate"], errors="coerce")
 
