be4bee4f74d129087be69bf8fc0c3a52854d3944
Xinxue (Shawn) Qu <1990qvxinxue@gmail.com>
2025-12-15 14:44:36 -0500

    Update 2_BellaVista.py

--- pages/2_BellaVista.py
+++ pages/2_BellaVista.py
@@ -11,7 +11,7 @@ st.write("This page lets you explore price distributions by zipcode and target a
 # -------------------- Load Data --------------------
 @st.cache_data
 def load_data():
-    xlsx_url = f"https://docs.google.com/spreadsheets/d/e/2PACX-1vQdtiJ3VFCxrdBNfKGJ-KjZasH7-wRJyZ0fgtbNIeYn4iRY4wl9Md2uWJ7deGINNIp2f9aPSBOI39aM/pub?output=xlsx"
+    xlsx_url = f"https://docs.google.com/spreadsheets/d/e/2PACX-1vSY3EsuoQPnflnp0mr-1E9EUD4OggkNqvMdxO7m3tAnrxn1u6u13_UXe7vRtU431g/pub?output=xlsx"
     
     usecols = [
         "ListAgentFullName","StandardStatus","DaysOnMarket", " OriginalListPrice",  #"pricing_accuracy", "ElementarySchool",
