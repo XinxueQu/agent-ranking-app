51ac7bdeadaa80de6019b958aa50ad6529855293
Xinxue (Shawn) Qu <1990qvxinxue@gmail.com>
2026-01-11 21:46:17 -0500

    Update agent_ranking_app.py

--- agent_ranking_app.py
+++ agent_ranking_app.py
@@ -55,7 +55,7 @@ def load_data():
         "ListAgentFullName","is_closed","DaysOnMarket","pricing_accuracy",
         "PostalCode","ClosePrice","ElementarySchool","SubdivisionName"
     ]
-    return pd.read_csv(url, usecols=usecols)
+    return pd.read_xlsx(url, usecols=usecols)
 
 data = load_data()
 
